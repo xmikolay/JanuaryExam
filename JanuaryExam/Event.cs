4f9f485bf5d4db8e989e14ec1876bd5bb46d0b19
MikolajMakoszewski-S <MikolajMakoszewski-S@S-CR1H504-SL29>
2025-01-08 13:30:21 +0000

    Created Event Class and Added IComparable Interface

---
+++ JanuaryExam/Event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam
+{
+    internal class Event : IComparable
+    {
+        public string Name { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public List<Ticket> Tickets { get; set; }
+
+        public enum EventType { Music, Comedy, Theatre}
+
+        public Event(string name, DateTime eventDate, List<Ticket> tickets)
+        {
+            Name = name;
+            EventDate = eventDate;
+            Tickets = tickets;
+        }
+
+        //IComparable interface
+        public int CompareTo(object obj)
+        {
+            Event that = obj as Event;
+
+            return this.EventDate.CompareTo(that.EventDate); //Comparing one event date to another
+        }
+    }
+}
--- JanuaryExam/MainWindow.xaml.cs
+++ JanuaryExam/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace JanuaryExam
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
